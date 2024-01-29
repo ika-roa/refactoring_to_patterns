bf908a4175d372370635408837b3935b8e809901
Stefanie Mak-Lucas <stefanie.mak-lucas@dynamic-biosensors.com>
2024-01-29 11:42:35 +0100

    test: Ensure that class is initialized correctly with string constructor

--- Python/creation_methods.py
+++ Python/creation_methods.py
@@ -1,15 +1,15 @@
 class Student:
-    def __init__(self, student_id, first_name):
+    def __init__(self, student_id: int, first_name: str):
         self.id = student_id
         self.first_name = first_name
 
     @classmethod
-    def from_string(cls, information_string):
+    def from_string(cls, information_string: str):
         student_id, name = information_string.split(", ")
-        return cls(student_id, name)
+        return cls(int(student_id), name)
 
     @classmethod
-    def from_list(cls, information_list):
+    def from_list(cls, information_list: list):
         student_id, name = information_list
         return cls(student_id, name)
 
--- Python/test/test_creation_methods.py
+++ Python/test/test_creation_methods.py
@@ -6,3 +6,8 @@ class TestCreationMethods:
         student = Student(1, "Bob")
         assert student.id == 1
         assert student.first_name == "Bob"
+
+    def test_that_class_is_initialized_correctly_with_string_constructor(self):
+        student = Student.from_string("1, Bob")
+        assert student.id == 1
+        assert student.first_name == "Bob"
