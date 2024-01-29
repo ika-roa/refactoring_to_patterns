09bef83383913ea5a35a1f71db583e697f148243
Stefanie Mak-Lucas <stefanie.mak-lucas@dynamic-biosensors.com>
2024-01-29 13:20:14 +0100

    refactor: Ensure that class is initialized correctly with custom constructor

--- Python/test/test_creation_methods.py
+++ Python/test/test_creation_methods.py
@@ -25,3 +25,10 @@ class TestTeacherWithConstructorOverloading:
         assert teacher.name == "Bob"
         assert teacher.has_car == False
         assert teacher.category == "junior"
+
+    def test_that_class_is_initialized_correctly_with_custom_constructor(self):
+        teacher = Teacher(teacher_id=2, name="John", has_car=True, category="senior")
+        assert teacher.id == 2
+        assert teacher.name == "John"
+        assert teacher.has_car == True
+        assert teacher.category == "senior"
