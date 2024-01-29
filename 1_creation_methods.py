11332560a97d94f717ae6638a17503b04ef0a474
Stefanie Mak-Lucas <stefanie.mak-lucas@dynamic-biosensors.com>
2024-01-29 08:19:07 +0100

    refactor: Add creation method for string input

--- 1_creation_methods.py
+++ 1_creation_methods.py
@@ -3,6 +3,11 @@ class Student:
         self.id = student_id
         self.first_name = first_name
 
+    @classmethod
+    def from_string(cls, information_string):
+        name_parts = information_string.split(", ")
+        return cls(name_parts[0], name_parts[1])
+
     def print_information(self):
         print(f"The student {self.first_name} has id {self.id}.")
 
@@ -11,3 +16,7 @@ if __name__ == "__main__":
     # example 1: Class called via normal constructor
     student_1 = Student(1, "John")
     student_1.print_information()
+
+    # example 2: Class called via alternate constructor
+    student_2 = Student.from_string("2, Bob")
+    student_2.print_information()
