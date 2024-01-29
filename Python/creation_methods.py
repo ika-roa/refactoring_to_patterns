ae0771f3143d85f772c0e44b3fc379ce619e1f54
Stefanie Mak-Lucas <stefanie.mak-lucas@dynamic-biosensors.com>
2024-01-29 11:43:37 +0100

    refactor: Remove unused code

--- Python/creation_methods.py
+++ Python/creation_methods.py
@@ -12,19 +12,3 @@ class Student:
     def from_list(cls, information_list: list):
         student_id, name = information_list
         return cls(student_id, name)
-
-    def print_information(self):
-        print(f"The student {self.first_name} has id {self.id}.")
-
-
-if __name__ == "__main__":
-    # Class called via normal constructor
-    student_1 = Student(1, "John")
-    student_1.print_information()
-
-    # Classes called via alternate constructors
-    student_2 = Student.from_string("2, Bob")
-    student_2.print_information()
-
-    student_3 = Student.from_list([3, "Judy"])
-    student_3.print_information()
