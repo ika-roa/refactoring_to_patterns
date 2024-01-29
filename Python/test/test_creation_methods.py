a78621d9d720f28d5cd177c4ee4a1c4e26a57516
Stefanie Mak-Lucas <stefanie.mak-lucas@dynamic-biosensors.com>
2024-01-29 11:40:27 +0100

    test: Ensure that class is initialized correctly with standard constructor

---
+++ Python/test/test_creation_methods.py
@@ -0,0 +1,8 @@
+from Python.creation_methods import Student
+
+
+class TestCreationMethods:
+    def test_that_class_is_initialized_correctly_with_standard_constructor(self):
+        student = Student(1, "Bob")
+        assert student.id == 1
+        assert student.first_name == "Bob"
