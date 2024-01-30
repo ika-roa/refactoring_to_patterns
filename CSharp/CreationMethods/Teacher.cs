76118e64dafadb878d76dd6f3a08fb3a310a3abc
Stefanie Mak-Lucas <stefanie.mak-lucas@dynamic-biosensors.com>
2024-01-30 08:11:45 +0100

    refactor: Create and test alternate constructors

--- CSharp/CreationMethods/Teacher.cs
+++ CSharp/CreationMethods/Teacher.cs
@@ -14,4 +14,20 @@ public class Teacher
         HasCar = hasCar;
         Category = category;
     }
+    
+    public Teacher(int id, string name, bool hasCar)
+    {
+        Id = id;
+        Name = name;
+        HasCar = hasCar;
+        Category = "senior";
+    }
+    
+    public Teacher(int id, string name, string category)
+    {
+        Id = id;
+        Name = name;
+        HasCar = true;
+        Category = category;
+    }
 }
