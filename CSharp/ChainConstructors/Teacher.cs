e52431100dbd19582666d65b0437b82ebe8af59d
Stefanie Mak-Lucas <stefanie.mak-lucas@dynamic-biosensors.com>
2024-01-30 14:25:04 +0100

    refactor: Add project and initial code for Chain Constructors refactoring

---
+++ CSharp/ChainConstructors/Teacher.cs
@@ -0,0 +1,33 @@
+﻿namespace ChainConstructors;
+
+public class Teacher
+{
+    public int Id { get; }
+    public string Name { get; }
+    public bool HasCar { get; }
+    public string Category { get; }
+
+    public Teacher(int id, string name, bool hasCar, string category)
+    {
+        Id = id;
+        Name = name;
+        HasCar = hasCar;
+        Category = category;
+    }
+    
+    public Teacher(int id, string name, bool hasCar)
+    {
+        Id = id;
+        Name = name;
+        HasCar = hasCar;
+        Category = "senior";
+    }
+    
+    public Teacher(int id, string name, string category)
+    {
+        Id = id;
+        Name = name;
+        HasCar = true;
+        Category = category;
+    }
+}
