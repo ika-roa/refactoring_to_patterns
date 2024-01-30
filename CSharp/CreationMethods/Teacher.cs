4c2b66d9e5e7027df079d68aaf7b9736e519ca6f
Stefanie Mak-Lucas <stefanie.mak-lucas@dynamic-biosensors.com>
2024-01-30 09:16:34 +0100

    refactor: Invoke overloaded constructor using “this” keyword

--- CSharp/CreationMethods/Teacher.cs
+++ CSharp/CreationMethods/Teacher.cs
@@ -15,14 +15,8 @@ public class Teacher
         Category = category;
     }
 
-    public Teacher(int id, string name, string category)
-    {
-        Id = id;
-        Name = name;
-        HasCar = true;
-        Category = category;
-    }
-
+    public Teacher(int id, string name, string category) : this(id, name, true, category){}
+    
     public static Teacher CreateSeniorTeacher(int id, string name, bool hasCar)
     {
         return new Teacher(id, name, hasCar, "senior");
