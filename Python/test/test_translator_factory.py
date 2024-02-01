39f2255826852287feeb90e86284630f9f22db38
Stefanie Mak-Lucas <stefanie.mak-lucas@dynamic-biosensors.com>
2024-02-01 08:11:19 +0100

    refactor: Create test classes

--- Python/test/test_translator_factory.py
+++ Python/test/test_translator_factory.py
@@ -1,49 +1,45 @@
 from Python.translator_factory import FrenchTranslator, app, SpanishTranslator, PortugueseTranslator, EnglishTranslator
 
 
-def test_that_english_translator_returns_correct_translation_for_car():
-    translator = EnglishTranslator()
-    translation = translator.translate("car")
-    assert translation == "car"
+class TestTranslator:
+    def test_that_english_translator_returns_correct_translation_for_car(self):
+        translator = EnglishTranslator()
+        translation = translator.translate("car")
+        assert translation == "car"
+
+    def test_that_french_translator_returns_correct_translation_for_car(self):
+        translator = FrenchTranslator()
+        translation = translator.translate("car")
+        assert translation == "voiture"
+
+    def test_that_spanish_translator_returns_correct_translation_for_car(self):
+        translator = SpanishTranslator()
+        translation = translator.translate("car")
+        assert translation == "coche"
+
+    def test_that_portuguese_translator_returns_correct_translation_for_car(self):
+        translator = PortugueseTranslator()
+        translation = translator.translate("car")
+        assert translation == "carro"
 
 
-def test_that_french_translator_returns_correct_translation_for_car():
-    translator = FrenchTranslator()
-    translation = translator.translate("car")
-    assert translation == "voiture"
+class TestApp:
+    def test_that_app_translates_to_english(self):
+        output = app("car", "english")
+        assert "-*-" in output
+        assert "car" in output
 
+    def test_that_app_translates_to_french(self):
+        output = app("car", "french")
+        assert "-*-" in output
+        assert "voiture" in output
 
-def test_that_spanish_translator_returns_correct_translation_for_car():
-    translator = SpanishTranslator()
-    translation = translator.translate("car")
-    assert translation == "coche"
+    def test_that_app_translates_to_spanish(self):
+        output = app("car", "spanish")
+        assert "-*-" in output
+        assert "coche" in output
 
-
-def test_that_portuguese_translator_returns_correct_translation_for_car():
-    translator = PortugueseTranslator()
-    translation = translator.translate("car")
-    assert translation == "carro"
-
-
-def test_that_app_translates_to_english():
-    output = app("car", "english")
-    assert "-*-" in output
-    assert "car" in output
-
-
-def test_that_app_translates_to_french():
-    output = app("car", "french")
-    assert "-*-" in output
-    assert "voiture" in output
-
-
-def test_that_app_translates_to_spanish():
-    output = app("car", "spanish")
-    assert "-*-" in output
-    assert "coche" in output
-
-
-def test_that_app_translates_to_portuguese():
-    output = app("car", "portuguese")
-    assert "-*-" in output
-    assert "carro" in output
+    def test_that_app_translates_to_portuguese(self):
+        output = app("car", "portuguese")
+        assert "-*-" in output
+        assert "carro" in output
