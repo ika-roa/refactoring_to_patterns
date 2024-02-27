2936248a1ade00032ab0739b23dc469f764702a3
Stefanie Mak-Lucas <stefanie.mak@dynamic-biosensors.com>
2024-02-27 08:17:25 +0100

    refactor: Initial state for strategy pattern

---
+++ CSharp/ReplaceConditionalLogicWithStrategy/FarmLand.cs
@@ -0,0 +1,84 @@
+﻿namespace ReplaceConditionalLogicWithStrategy;
+
+public class FarmLand
+{
+    private Type _type = Type.Garden;
+
+    public string Describe()
+    {
+        if (_type == Type.Field)
+            return "Field";
+        if (_type == Type.Garden)
+            return "Garden";
+        if (_type == Type.Orchard)
+            return "Orchard";
+        return "";
+    }
+
+    public void SetType(Type type)
+    {
+        _type = type;
+    }
+
+    public double CalculateYield(int numberOfWorkers, int amountOfRain = 0)
+    {
+        const double baseYield = 2;
+        double yield = 0;
+
+        if (_type == Type.Field)
+        {
+            if (amountOfRain > 10)
+            {
+                yield = baseYield * 2;
+            }
+            else if (amountOfRain > 5)
+            {
+                yield = baseYield;
+            }
+            else
+            {
+                yield = baseYield / 2;
+            }
+        }
+
+        if (_type == Type.Garden)
+        {
+            if (numberOfWorkers > 5)
+            {
+                yield = baseYield * 10;
+            }
+            else
+            {
+                yield = baseYield / 10;
+            }
+        }
+
+        if (_type == Type.Orchard)
+        {
+            if (numberOfWorkers > 2)
+            {
+                if (amountOfRain > 5)
+                {
+                    yield = baseYield * 5;
+                }
+                else
+                {
+                    yield = baseYield;
+                }
+            }
+            else
+            {
+                yield = 0;
+            }
+        }
+
+        return yield;
+    }
+}
+
+public enum Type
+{
+    Field,
+    Garden,
+    Orchard
+}
